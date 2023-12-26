2fdb34f8262534fcd25b6d8d82285da9c76ef1fb
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2023-12-26 16:49:21 +0100

    randomize basics

--- site/src/app/input/input.component.ts
+++ site/src/app/input/input.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
 
   placeholder = '3WIAI9I427fHtRH';
+
+  randomize(): void{
+    this.placeholder = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
 
 }
